ab45079a8107fb607a4c057c1a9c0358ff0783ff
Liam North <liam_north@outlook.com>
2017-11-03 16:53:55 +0000

    current_path changed to current_url in basket feature test

--- features/step_definitions/zeitgeist_basket_steps.rb
+++ features/step_definitions/zeitgeist_basket_steps.rb
@@ -8,7 +8,7 @@ When(/^I click on the name of an item in my basket$/) do
   @zeitgeist_site.zeitgeist_cart_page.click_on_product_name_link
 end
 Then(/^I am redirected to that item's product page$/) do
-  expect(current_path).to include('/product/')
+  expect(current_url).to include('/product/')
 end
 
 
